0aec1820d8968d1aceda63b7bf2242d95fe3a173
GDuang <2418625613@qq.com>
2024-02-26 18:16:34 +0800

    24.02.26.2

--- FlowCountNode/flow_count.js
+++ FlowCountNode/flow_count.js
@@ -10,9 +10,14 @@ module.exports = function(RED) {
         var count = 0;
 
 
+        const pattern = /^[0-9]+$/;
+        const isAllDigits = pattern.test(node.num_Init);  // 为真表示字符串全为数字组成
+
         // 若勾选了引脚电平初始化
-        if (node.set == true) {
+        if (node.set == true && isAllDigits) {
+            count = parseInt(node.num_Init)
             msg = {'topic': "", 'payload': node.num_Init}
+            node.status({fill: "green",shape: "ring",text: `当前计数：${count}`});
             node.send(msg);
         }
 
@@ -21,6 +26,7 @@ module.exports = function(RED) {
           count++; // 计数自加
     
           msg.payload = count; // 将计数值添加到消息中
+          node.status({fill: "green",shape: "ring",text: `当前计数：${count}`});
     
           node.send(msg); // 将消息发送到下游节点
         });
